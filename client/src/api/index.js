3b4ae408ef0ab9d54e93c0408a7374b2c9af0db6
Swapnil Raj <swapnilraj85444@gmail.com>
2025-05-12 22:44:44 +0530

    Final changes done

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://image-generator-mern-uwfg.onrender.com/api/post",
 });
 
 export const GetPosts = async () => await API.get("/post/");
